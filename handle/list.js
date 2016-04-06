8f7018eab61abd193a6324489a449d67ebdfaec5
techird <techirdliu@gmail.com>
2016-04-06 23:50:33 +0800

    Step6. 添加分页支持和搜索到列表上

--- handle/list.js
+++ handle/list.js
@@ -19,9 +19,38 @@ const list = (request, response) => async (function * () {
     
     try {
         const imageCollection = db.collection("images");
-        let query = imageCollection.find();
+        
+        let criteria = {};
+        
+        // build search criteria
+        const keyword = params.search;
+        if (keyword) {
+            const search = new RegExp(keyword);
+            console.log("search: " + keyword);
+            criteria.$or = ['name', 'meta.author', 'meta.labels', 'meta.alt'].map(field => ({ [field]: search }));
+        }
+        
+        const total = yield imageCollection.count(criteria);
+        
+        let query = imageCollection.find(criteria);
+        let pageIndex = +params.pageIndex;
+        let pageSize = +params.pageSize;
+        
+        // see if user has request for a paging
+        if (!isNaN(pageIndex) && !isNaN(pageSize)) {
+            pageIndex = pageIndex || 0;
+            pageSize = pageSize || 10;
+            const start = pageIndex * pageSize;
+            query = query.skip(start).limit(pageSize);
+        }
+        
         const list = yield query.toArray();
-        print({ list });
+            
+        print({
+            total,
+            list, 
+            paging: { pageIndex, pageSize } 
+        });
     } catch (error) {
         console.log(error);
         print({ error });
